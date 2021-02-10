d4cb177a54d9b6d8a529aa9a94843f5dc31e3c8c
luizjuniordeveloper <luizjuniordeveloper@gmail.com>
2021-02-10 00:40:30 -0300

    criação dos modelos

--- LProject/Locadora/models.py
+++ LProject/Locadora/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Titulo(models.Model):
+    nome = models.CharField()
+    ano = models.IntergerField()
+    studio = models.CharField()
+    preco = models.FloatField()
+
+class Fita(models.Model):
+    title = models.ForeignKey(Titulo, related_name='fitas', on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+
+
+class Locacao(models.Model):
+    data_locacao = models.DataTimeField(auto_now_add=True, blank=True)
+    data_entrega =  models.DataTimeField()
+    fitas = models.ManyToManyFields(Fita, related_name='fitas', on_delete=models.CASCADE)
+    socio = models.ForeignKey(User, related_name='locacoes',on_delete=models.CASCADE)
+    valor_total =  models.FloatField()
